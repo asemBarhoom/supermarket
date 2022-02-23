5538b6afd2703d2efae7474a14613c7258403c72
asem barhoom <atbarhoom@hotmail.com>
2022-02-23 11:44:24 +0200

    format code

--- app/Http/Controllers/ChekoutController.php
+++ app/Http/Controllers/ChekoutController.php
@@ -10,8 +10,8 @@ class ChekoutController extends Controller
     public function scan(Request $request)
     {
         $request->validate([
-            'products'=>'required',
-            'products.*'=>'required|exists:products,product_code',
+            'products' => 'required',
+            'products.*' => 'required|exists:products,product_code',
         ]);
 
         $total = 0;
@@ -19,24 +19,22 @@ class ChekoutController extends Controller
         foreach ($request->products as $product) {
 
             $qty = $request->qty[$product][0];
-            $product = Product::where('product_code',$product)->first();
-            $price  = $product->price;
+            $product = Product::where('product_code', $product)->first();
+            $price = $product->price;
 
-            if ($product->product_code == Product::FR1_TYPE && $qty > 1 )
-            {
+            if ($product->product_code == Product::FR1_TYPE && $qty > 1) {
 
-                $qty = ($qty%2==0)? $qty/2 : $qty - $qty%2; // 1.	If you buy one Fruit Tea get another for free
+                $qty = ($qty % 2 == 0) ? $qty / 2 : $qty - $qty % 2; // 1.	If you buy one Fruit Tea get another for free
             }
 
-            if ($product->product_code == Product::SR1_TYPE && $qty>=3)
-            {
+            if ($product->product_code == Product::SR1_TYPE && $qty >= 3) {
                 $price = 4.5; //2.	If you buy 3 or more Strawberry price drop for $4.5
             }
 
-            $total += $qty*$price;
+            $total += $qty * $price;
         }
 
-        return view('product.index',['total'=>$total,'products'=>Product::get(),'selectedProducts'=>$request->products,'qty'=>$request->qty]);
+        return view('product.index', ['total' => $total, 'products' => Product::get(), 'selectedProducts' => $request->products, 'qty' => $request->qty]);
 
     }
 }
